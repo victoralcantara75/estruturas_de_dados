9725ae01b70192814555c5cbae3f65ec95220065
victor alcantara <victorjnio75@gmail.com>
2019-01-04 15:52:52 -0200

    pilha

---
+++ PILHA/pilha_dinamica.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct sCell{
+    int info;
+    struct sCell *prox;
+}celula;
+
+typedef struct sPilha{
+    celula *ptrtopo;
+    int tamanho;
+}pilha;
+
+void* criarCelula(){
+    return (celula*)malloc(sizeof(celula));
+}
+
+void inicializar(pilha *p){
+    p->ptrtopo = NULL;
+    p->tamanho = 0;
+}
+
+int vazia(pilha *p){
+    if(p->ptrtopo == NULL)
+        return 1;
+    return 0;
+}
+
+int push(pilha *p, int elem){
+    
+    celula *no = criarCelula();
+    no->info = elem;
+
+    no->prox = p->ptrtopo;
+    p->ptrtopo = no;
+    p->tamanho++;
+
+    return 1;
+}
+
+int pop(pilha *p){
+    if(vazia(p))
+    {
+        printf("não há elementos para desempilhar\n");
+        return 0;
+    }
+    
+    celula *aux = p->ptrtopo;
+    p->ptrtopo = p->ptrtopo->prox;
+    free(aux);
+}
+
+void imprimir(pilha *p){
+    while(!vazia(p)){
+        printf("%d ", p->ptrtopo->info);
+        pop(p);
+    }
+    printf("\n");
+}
+
+int main(){
+    
+    pilha p;
+    inicializar(&p);
+    vazia(&p);
+
+    push(&p, 5);
+    push(&p, 8);
+
+    imprimir(&p);
+
+    return 0;
+}
---
+++ PILHA/pilha_estatica.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#define TAM 10
+
+typedef struct sPilha{
+    int info[TAM];
+    int topo;
+}pilha;
+
+void inicializar(pilha *p){
+    p->topo = -1;
+}
+
+int vazia(pilha *p){
+    if(p->topo == -1)
+        return 1;
+    return 0;
+}
+
+int cheia(pilha *p){
+    if(p->topo == TAM-1)
+        return 1;
+    return 0;
+}
+
+int push(pilha *p, int elem){
+    if(cheia(p)){
+        printf("pilha cheia!\n");
+        return 0;
+    }
+
+    p->topo++;
+    p->info[p->topo] = elem;
+    return 1;
+}
+
+int pop(pilha *p){
+    if(vazia(p)){
+        printf("pilha vazia!\n");
+        return 0;
+    }
+    p->topo--;
+    return 1;
+}
+
+void imprimirNaoClassico(pilha *p){
+    for(int i=0; i<p->topo; i++)
+        printf("%d ", p->info[i]);
+
+    printf("\n");
+}
+
+int main(){
+
+    pilha p;
+    inicializar(&p);
+
+    for(int i=0; i<10; i++){
+        push(&p, i);
+    }
+
+    for(int i=0; i<7; i++){
+        pop(&p);  
+    }
+    imprimirNaoClassico(&p);
+    return 0;
+}
